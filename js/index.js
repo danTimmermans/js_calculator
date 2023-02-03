8ab359297612eb73fc0fb66c807f83987366a9c0
unknown <dantim1974@hotmail.com>
2023-02-03 19:43:03 +0100

    fix responsive4

--- js/index.js
+++ js/index.js
@@ -14,10 +14,6 @@ function calculate(button){
         calculation.push(value);
         accumulativeCalculation = calculation.join('');
         screenDisplay.textContent = accumulativeCalculation;
-    }
-
-
-  
-   
+    }   
 }
 buttons.forEach(button => button.addEventListener('click', ()=>calculate(button)))
